63d7c47df4a17d99e158866c8f7593b8b7ff9943
DashJay <dashjay@aliyun.com>
2020-03-30 17:14:54 +0800

    finishi all basic game

--- main.go
+++ main.go
@@ -1,13 +1,55 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/dashjay/gobang/windows"
 )
 
+const (
+	InputFormatError = "输入的格式有误，请重新输入 「x,y」"
+)
+
 func main() {
+	// When change turn we can use this arr to help
+	changeTurn := []int{0, 2, 1}
 
-	g := windows.NewGame(10, 10)
-	g.Point(5, 4)
-	g.Print()
+	// start a game width:10 height:10 player1 first TODO: player0 will undertake by AI
+	tryGame := windows.NewGame(10, 10, 1)
+	// player1 play
+	for {
 
+		fmt.Printf("轮到玩家%d落子\n", tryGame.Turn)
+		fmt.Print("\r" + tryGame.Print())
+		var (
+			res int
+			x   int
+			y   int
+		)
+
+		// read x,y from stdin
+		_, err := fmt.Scanf("%d,%d", &x, &y)
+		if err != nil {
+			fmt.Println(InputFormatError, err)
+			continue
+		}
+
+		// point one on x,y
+		err = tryGame.Point(x, y)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		// check if someone win the game
+		res = tryGame.WinGame(x, y)
+
+		// 没有输赢
+		if res == -1 {
+			tryGame.Turn = changeTurn[tryGame.Turn]
+			continue
+		}
+		fmt.Printf("玩家%d已经赢了", res)
+		return
+	}
 }
--- windows/game.go
+++ windows/game.go
@@ -1,18 +1,40 @@
 package windows
 
 import (
+	"errors"
 	"fmt"
+	"regexp"
 	"strings"
 )
 
+//  Game 一个游戏
 type Game struct {
-	str    []string
-	Width  int
+	// 游戏所使用的字符集 例如 .代表空 @代表player1 #代表player2
+	Str []string
+	// 宽度 x 轴
+	Width int
+	// 高度 y 轴
 	Height int
-	Board  [][]int
+	// 棋盘
+	Board [][]int
+	// 当前游戏轮到谁
+	Turn int
 }
 
-func NewGame(w, h int) *Game {
+// scoreArr 当有0,1个子是毫无意义的,
+// 当有2个连起来对应50分
+//    3个连起来对应200分
+// 当有4个连起来对应3000分
+// 5 -> 999999999
+var scoreArr = []int{0, 0, 50, 200, 3000, 2 << 28}
+
+// start a game w: width
+// h: height
+// first 0 or 1
+func NewGame(w, h, first int) *Game {
+	if first != 1 && first != 2 {
+		panic(fmt.Sprintf("先手只能为1或2你传入了%d", first))
+	}
 
 	// init board ptr
 	var board [][]int
@@ -27,12 +49,14 @@ func NewGame(w, h int) *Game {
 		Width:  w,
 		Height: h,
 		Board:  board,
-		str:    []string{" . ", " * ", " # "},
+		Str:    []string{" . ", " @ ", " # "},
+		Turn:   first, // who play first
 	}
 
 	return &g
 }
 
+// print the board
 func (g *Game) Print() string {
 	// header line like this
 	//  x  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20x轴
@@ -69,7 +93,7 @@ func (g *Game) Print() string {
 		}
 		// write state
 		for tempx := 1; tempx <= g.Width; tempx++ {
-			output.WriteString(g.str[g.Board[tempx][tempy]])
+			output.WriteString(g.Str[g.Board[tempx][tempy]])
 		}
 		// line end
 		output.WriteString("\n")
@@ -81,10 +105,129 @@ func (g *Game) Print() string {
 	return header.String() + output.String()
 }
 
-func (g *Game) Point(x, y int) {
+var (
+	ErrorOverFlow  = errors.New("point over the width or height")
+	HasBeenTakenUp = errors.New("point has been taken up")
+)
+
+// Point point one at x, y
+func (g *Game) Point(x, y int) error {
+	// 超过了边界
 	if x > g.Width || y > g.Height || x < 0 || y < 0 {
-		panic("x y error")
-	} else {
-		g.Board[x][y] = 1
+		return ErrorOverFlow
 	}
+	// 已经被占用
+	if g.Board[x][y] != 0 {
+		return HasBeenTakenUp
+	}
+	g.Board[x][y] = g.Turn
+	return nil
+}
+
+// WinGame who wins the game ?
+// pass in x,y current position
+func (g *Game) WinGame(x, y int) int {
+
+	// 赢的条件
+	Win := regexp.MustCompile(fmt.Sprintf(`%s{5}`, strings.TrimSpace(g.Str[g.Turn])))
+
+	var (
+		// from left to right
+		buf21 strings.Builder
+		// from up to down
+		buf14 strings.Builder
+		// // from right up to left down
+		buf13 strings.Builder
+		// // from left up to right down
+		buf24 strings.Builder
+	)
+	// pin y, range x from 1 to width
+	for tempx := 1; tempx <= g.Width; tempx++ {
+		buf21.WriteString(strings.TrimSpace(g.Str[g.Board[tempx][y]]))
+	}
+
+	if Win.MatchString(buf21.String()) {
+		return g.Turn
+	}
+
+	for tempy := 1; tempy <= g.Height; tempy++ {
+		buf14.WriteString(strings.TrimSpace(g.Str[g.Board[x][tempy]]))
+	}
+
+	if Win.MatchString(buf14.String()) {
+		return g.Turn
+	}
+	var (
+		tempx = x
+		tempy = y
+	)
+	// 寻到这条线左上角的点
+	for ; tempx > 1 && tempy > 1; {
+		tempx--
+		tempy--
+	}
+	// 获取所有字符
+	for ; tempx < g.Width && tempy < g.Height; {
+		buf24.WriteString(strings.TrimSpace(g.Str[g.Board[tempx][tempy]]))
+		// 增加
+		tempx++
+		tempy++
+	}
+	// 判断输赢
+	if Win.MatchString(buf24.String()) {
+		return g.Turn
+	}
+
+	tempx = x
+	tempy = y
+	// 寻找到右上角的点
+	for ; tempx < g.Width && tempy > 1; {
+		tempx++
+		tempy--
+	}
+
+	for ; tempx > 1 && tempy < g.Height; {
+		buf13.WriteString(strings.TrimSpace(g.Str[g.Board[tempx][tempy]]))
+		tempx--
+		tempy++
+	}
+	if Win.MatchString(buf13.String()) {
+		return g.Turn
+	}
+
+	return -1
+}
+
+// GetScore 得到某条线上的得分~
+func (g *Game) GetScore(line string) int {
+
+	var (
+		MyScore       = 0
+		OppositeScore = 0
+		MyCount       = 0
+		OppositeCount = 0
+	)
+	opposite := []int{0, 2, 1}
+
+	for i := 5; i >= 2; i-- {
+
+		// 我是的得分正则 如果我是 1那么对手就是 2
+		MyWin := regexp.MustCompile(fmt.Sprintf(`%s{%d}`, strings.TrimSpace(g.Str[g.Turn]), i))
+
+		// 寻找我的连续 n 颗棋子
+		tempMyCount := len(MyWin.FindAllStringSubmatch(line, -1))
+		tempMyCount1 := tempMyCount - MyCount
+		MyScore += tempMyCount1 * scoreArr[i]
+		MyCount += tempMyCount1
+
+		// 寻找对手的连续 n 颗棋子
+		OppositeWin := regexp.MustCompile(fmt.Sprintf(`%s{%d}`, strings.TrimSpace(g.Str[opposite[g.Turn]]), i))
+		tempOppositeCount := len(OppositeWin.FindAllStringSubmatch(line, -1))
+		tempOppositeCount1 := tempOppositeCount - OppositeCount
+		OppositeScore += tempOppositeCount1 * scoreArr[i]
+		OppositeCount += OppositeScore
+
+	}
+
+	return MyScore - OppositeScore
 }
