c762efd1ca757f700f93f5fc188db7bc33b62f53
DashJay <dashjay@aliyun.com>
2020-03-30 17:16:51 +0800

    not finish ai but we can use go run main.go to get it work

--- ai/ai.go
+++ ai/ai.go
@@ -1,1 +1,68 @@
 package ai
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/dashjay/gobang/windows"
+)
+
+var (
+	cutCount    int
+	searchCount int
+)
+
+// 必须把当前整个游戏传进来
+func AI(d windows.Game) (x, y int) {
+	// 初始化
+
+	cutCount = 0
+	searchCount = 0
+
+	// 0 曾搜索测试
+	fmt.Println(NegMax(&d, 0, -99999999, 99999999))
+
+	fmt.Println("剪枝", cutCount)
+	fmt.Println("搜索", searchCount)
+	return 0, 0
+}
+
+func NegMax(game *windows.Game, depth, alpha, beta int) int {
+	if depth == 0 {
+		fmt.Println("depth 0")
+		return Evaluation(game)
+	}
+
+	for x := 0; x <= game.Width; x++ {
+		for y := 0; y <= game.Height; y++ {
+			res := game.Point(x, y)
+		}
+	}
+	return 0
+}
+
+//
+func Evaluation(game *windows.Game) int {
+
+	var totalScore = 0
+
+	for y := 1; y < game.Height; y++ {
+		var line strings.Builder
+		// 收集一条line
+		for x := 1; x < game.Width; x++ {
+			line.WriteString(strings.TrimSpace(game.Str[game.Board[x][y]]))
+		}
+		totalScore += game.GetScore(line.String())
+	}
+
+	for x := 1; x < game.Width; x++ {
+		var line strings.Builder
+		for y := 1; y < game.Height; y++ {
+			line.WriteString(strings.TrimSpace(game.Str[game.Board[x][y]]))
+		}
+		totalScore += game.GetScore(line.String())
+	}
+
+	fmt.Println(game.Print())
+	return totalScore
+}
--- game_test.go
+++ game_test.go
@@ -2,25 +2,107 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
+	"github.com/dashjay/gobang/ai"
 	"github.com/dashjay/gobang/windows"
 )
 
 func TestWindowGame(t *testing.T) {
 	t.Log("test w > h")
-	a := windows.NewGame(10, 20)
+	a := windows.NewGame(10, 20, 1)
 	fmt.Print(a.Print())
 
 	t.Log("test w < h")
-	b := windows.NewGame(20, 10)
+	b := windows.NewGame(20, 10, 1)
 	fmt.Print(b.Print())
 
 	t.Log("test w > h and play some chess")
-	c := windows.NewGame(10, 20)
+	c := windows.NewGame(10, 20, 1)
 	c.Point(5, 4)
 	c.Point(4, 5)
 	c.Point(2, 3)
 	fmt.Print(c.Print())
 	t.Log("it works!!")
+}
+
+func TestWinGame(t *testing.T) {
+	var res int
+	// vertical win
+	a := windows.NewGame(20, 10, 1)
+	for _, i := range []int{5, 6, 7, 8, 9} {
+		err := a.Point(1, i)
+		if err != nil {
+			
+		}
+	}
+
+	fmt.Print(a.Print())
+	fmt.Println(res)
+
+	// Horizontal win
+	b := windows.NewGame(10, 20, 1)
+	b.Point(2, 5)
+	b.Point(3, 5)
+	b.Point(4, 5)
+	b.Point(5, 5)
+	b.Point(6, 5)
+	fmt.Println(b.Print())
+	fmt.Print(a.Print())
+	fmt.Println(res)
+
+	// left up to right down win
+	c := windows.NewGame(20, 10, 1)
+	for _, i := range []int{1, 2, 3, 4, 5} {
+		res = c.Point(i, i)
+	}
+	fmt.Print(a.Print())
+	fmt.Println(res)
+
+	// right up to left down win
+	d := windows.NewGame(20, 20, 1)
+	d.Point(7, 2)
+	d.Point(6, 3)
+	d.Point(5, 4)
+	d.Point(4, 5)
+	res = d.Point(3, 6)
+
+	fmt.Print(d.Print())
+	fmt.Println(res)
+}
+
+func TestWinRegexp(t *testing.T) {
+	win := "...*#*#*#...*****"
+	res, _ := regexp.Compile(`\*{5}`)
+	t.Log(res.MatchString(win))
+}
+
+func TestScoreRegexp(t *testing.T) {
+	var score = []int{0, 0, 50, 200, 3000, 999999999}
+
+	// line := "..@..@@@.@@@.@@@@@." // 1000003799
+	// line := "..@..@@@.@@@.@@@@." // 3800
+	line := "..@..@@.@@@.@@@." // 750
+	getScore := 0
+	var o int
+	// 				  2, 3, 4, 5
+	for i := 5; i >= 2; i-- {
+		reg := regexp.MustCompile(fmt.Sprintf(`@{%d}`, i))
+		temp := len(reg.FindAllStringSubmatch(line, -1))
+		temp1 := temp - o
+		getScore += temp1 * score[i]
+		o += temp - temp1
+	}
+	fmt.Println(getScore)
+}
+
+func TestRunAI(t *testing.T) {
+	g := windows.NewGame(10, 10, 1)
+	g.Point(1, 1)
+	g.Point(2, 2)
+	g.Point(3, 3)
+	g.Point(4, 4)
+	g.Point(5, 5)
+	ai.AI(*g)
 }
